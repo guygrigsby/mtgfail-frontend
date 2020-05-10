0e01469fd004fd3283d4b4cfd1b699cf88953919
Guy J Grigsby <guy.grigsby@gmail.com>
2020-05-09 19:14:22 -0600

    readme badge

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,48 @@
 import React, { useState, Component } from 'react';
 
-import ReactBootstrap, {Container, Nav, Navbar, NavDropdown, Button} from 'react-bootstrap'
+import ReactBootstrap, {Container, Nav, Navbar, NavDropdown, Button, Modal} from 'react-bootstrap'
 
 const NavBar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
 
     <Container>
       <Navbar bg="primary" variant="dark">
         <Navbar.Brand href="#home">mtg.fail</Navbar.Brand>
-         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse className="justify-content-end">
           <Nav className="justify-content-end">
             <NavDropdown title="fail so hard" id="collasible-nav-dropdown">
               <NavDropdown.Item href="mailto:devs@mtg.fail">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/guygrigsby/mtgfail" target="_blank">Contribute</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Donate</NavDropdown.Item>
+
+              <NavDropdown.Item onClick={handleShow}>Donate</NavDropdown.Item>
+
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Donate</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          BTC
+          bc1qw07j7spyavapez2kakknmu4k0dtftl57mrzup4
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
     </Container>
   );
 }
